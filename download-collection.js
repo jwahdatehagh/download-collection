e5b7d589c56ab1ef9fb4d621d3a16ab94b952445
jalil <jalil@1001.digital>
2022-02-16 15:39:51 +0100

    Fix script

--- download-collection.js
+++ download-collection.js
@@ -1,15 +1,16 @@
 import fs from 'fs'
 import fetch from 'node-fetch'
 
-const [ URL ] = process.argv.slice(2)
+const [ URL, START, END ] = process.argv.slice(2)
 
 export const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
 
 const collection = []
 
-let id = 1;
+let id = parseInt(START)
+const last = parseInt(END);
 (async () => {
-  while (true) {
+  while (id <= last) {
     try {
       let i = id
       const result = await fetch(`${URL}/${i}`)
@@ -19,8 +20,7 @@ let id = 1;
       id ++
     } catch (e) {
       console.warn(e.message)
-      console.warn(`Fetch for token #${id} failed. Stopping execution and saving collection`)
-      fs.writeFileSync('collection.json', JSON.stringify(collection, null, 4))
     }
   }
+  fs.writeFileSync('collection.json', JSON.stringify(collection, null, 4))
 })()
